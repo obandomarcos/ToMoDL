f3bafa2ce4ebc8a889e0e00c76f50cb3adbda64f
marcoso96 <obandomarcos96@gmail.com>
2021-09-22 19:22:03 -0300

    Update: Images get obscure

--- Training/Training_projections.py
+++ Training/Training_projections.py
@@ -34,9 +34,9 @@ train_dataset, test_dataset = modutils.formRegDatasets(folder_paths, umbral_reg)
 # Training with more than one dataset
 number_projections = [72]
 
-train_size = 900
-val_size = 100
-test_size = 100
+train_size = 1000
+val_size = 200
+test_size = 200
 
 batch_size = 5
 img_size = 100
@@ -52,26 +52,26 @@ for proj_num in number_projections:
     #%% Model Settings
     nLayer = 3
     K = 5
-    epochs = 20
-    lam = 0.05
+    epochs = 50
+    lam = 0.1
     max_angle = 720
     
-    model = modl.OPTmodl(nLayer, K, proj_num, img_size, None, lam)
+    model = modl.OPTmodl(nLayer, K, max_angle, img_size, None, lam)
     loss_fn = torch.nn.MSELoss(reduction = 'sum')
     loss_fbp_fn = torch.nn.MSELoss(reduction = 'sum') 
-    lr = 1e-3
-    optimizer = torch.optim.RMSprop(model.parameters())
+    lr = 1e-4
+    optimizer = torch.optim.Adam(model.parameters(), lr = lr)
     
     ### Training
     model, train_info = modutils.model_training(model, loss_fn, loss_fbp_fn, optimizer, dataloaders, device, results_folder, num_epochs = epochs, disp = True, do_checkpoint = 0)
      
     train_infos[proj_num] = train_info
     
-    print('Test MODL loss {}'.format(train_infos[proj_num]['train'][-1]))
-    print('Test FBP loss {}'.format(train_infos[proj_num]['train_fbp'][-1]))
+    print('Train MODL loss {}'.format(train_infos[proj_num]['train'][-1]))
+    print('Train FBP loss {}'.format(train_infos[proj_num]['train_fbp'][-1]))
 
     #%% save loss for fbp and modl network
-    with open(results_folder+'FBP_error_projections_nlay{}_epochs{}_K{}_lam{}.pkl'.format(nLayer, epochs, K, lam), 'wb') as f:
+    with open(results_folder+'FBP_error_projections_Proj{}_nlay{}_epochs{}_K{}_lam{}.pkl'.format(proj_num, nLayer, epochs, K, lam), 'wb') as f:
     
         pickle.dump(train_infos, f)
         print('Diccionario salvado para proyección {}'.format(proj_num))
@@ -95,7 +95,7 @@ for proj_num in number_projections:
 
     test_loss_dict[proj_num] = {'loss_net': test_loss_total, 'loss_fbp': test_loss_fbp_total}
 
-    with open(results_folder+'Unique_Proj{}_nLay{}_K{}_lam{}.pkl'.format(proj_num, nLayer, K, lam), 'wb') as f:
+    with open(results_folder+'Unique_Proj{}_nLay{}_epochs{}_K{}_lam{}.pkl'.format(proj_num, nLayer, epochs, K, lam), 'wb') as f:
         
         pickle.dump(test_loss_dict, f)
         print('Diccionario salvado para proyección {}'.format(proj_num))

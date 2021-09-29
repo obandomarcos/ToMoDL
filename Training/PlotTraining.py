cdbfbf8e0904bca9bb1e6a1ebedc52e649681270
marcoso96 <obandomarcos96@gmail.com>
2021-09-29 16:01:48 -0300

    Plot training script

--- Training/PlotTraining.py
+++ Training/PlotTraining.py
@@ -1,40 +1,33 @@
-import pickle 
+import pickle
+import os, sys
 os.chdir('/home/marcos/DeepOPT/')
 sys.path.append('Utilities/')
 sys.path.append('OPTmodl/')
 import matplotlib.pyplot as plt
 import numpy as np
-from folders_cluster
+from Folders_cluster import *
+import ModelUtilities as modutils
+nLayer = 3
+K = 5
+epochs = 40
+lam = 0.05
+max_angle = 720
+proj_num = 72
+img_size = 100
+train_size = 150
 
-with open(results_folder+'FBP_error_projections.pkl', 'rb')
+with open(results_folder+'FBP_error_projections_Proj{}_nlay{}_epochs{}_K{}_lam{}_trnSize{}.pkl'.format(proj_num, nLayer, epochs, K, lam, train_size), 'rb') as f:
     train_info = pickle.load(f)
-
-projections = [20, 30, 40, 60, 90, 120, 180]
-error_test = []
-error_test_std = []
-
-error_fbp_test = []
-error_fbp_test_std = []
-
-for train_proj in train_info.values():
-    
-   error_test.append(train_proj['test'][-1])
-   #error_test_std.append(train_proj['test_std'][-1])
    
-   error_fbp_test.append(train_proj['test_fbp'][-1])
-   error_fbp_test_std.append(train_proj['test_fbp_std'][-1])
-
 #%% Plot
-fig, ax = plt.subplots(1,2, figsize = (8,6))
+fig, ax = plt.subplots(1,1, figsize = (8,6))
 
-ax.errorbar([i for i in range(len(projections))], error_test, yerr=error_test_std, fmt = '*', markersize = 8, label = 'Error MODL')
-ax.grid()
-ax.errorbar([i for i in range(len(projections))], error_fbp_test, yerr=error_fbp_test_std, fmt = '*', markersize = 8, label = 'Error FBP')
-ax.set_xticks([i-1 for i in range(len(projections)+1)])
-ax.set_xlim([-1, len(projections)])
-ax.set_xlabel('# of projections', fontsize = 14)
-ax.set_ylabel('MSE', fontsize = 14)
-ax.set_xticklabels([0]+list(dict.fromkeys(projections)))
+ax.plot(modutils.psnr(img_size, train_info[proj_num]['train']), label='train loss')
+ax.plot(modutils.psnr(img_size,train_info[proj_num]['val']), label = 'validation loss')
+ax.plot(modutils.psnr(img_size,train_info[proj_num]['train_fbp']), label = 'FBP loss benchmark', linestyle = '--')
+ax.set_xlabel('Epochs')
+ax.set_ylabel('PSNR [dB]')
+ax.legend()
+ax.grid(True)
 
-fig.savefig()
-
+fig.savefig(results_folder+'PlotLoss_Proj{}_nLay{}_epochs{}_K{}_lam{}_trnSize{}.pdf'.format(proj_num, nLayer, epochs, K, lam, train_size))

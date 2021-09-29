25b94549320b423288bbde0177f6df5b1d4dc985
marcoso96 <obandomarcos96@gmail.com>
2021-09-29 15:38:15 -0300

    Training_Unet

---
+++ Training/Training_Unet.py
@@ -0,0 +1,28 @@
+"""
+Train the model with different projections angles
+author: obanmarcos
+"""
+import os
+import os,time, sys
+os.chdir('/home/marcos/DeepOPT/')
+sys.path.append('Utilities/')
+sys.path.append('OPTmodl/')
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import pandas as pd
+import DataLoading as DL
+from Folders_cluster import *
+import ModelUtilities as modutils
+import torch
+from torch_radon import Radon, RadonFanbeam
+from skimage.transform import radon, iradon
+import phantominator as ph
+import torchvision
+import model_torch as modl
+import pickle
+from tqdm import tqdm
+
+# Using CPU or GPU
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+folder_paths = [ f140315_3dpf, f140419_5dpf, f140115_1dpf,f140714_5dpf] # Folders to be used

00c3dbe97545f2520912352bb8c620ab3715b22d
marcoso96 <obandomarcos96@gmail.com>
2021-11-04 12:13:19 -0300

    Change dataloading to presaved tensor

--- Training/Training_FastInit.py
+++ Training/Training_FastInit.py
@@ -30,35 +30,40 @@ umbral_reg = 50
 
 #%% Datasets 
 # Training with more than one dataset
-proj_num = 72
 
-train_size = 100
-val_size = 20
-test_size = 20
-
+number_projections = 72
+total_size = 2000
+train_factor = 0.7
+val_factor = 0.2
+test_factor = 0.1
 batch_size = 5
 img_size = 100
-augment_factor = 15
+projections_augment_factor = 3
+transform_augment_factor = 3
+
+tensor_path = datasets_folder+'Proj_{}_size_{}_imgsize_{}_projaugment_{}_transformaugment_{}_'.format(number_projections, total_size, img_size, projections_augment_factor, transform_augment_factor)
+
+#datasets = modutils.formRegDatasets(folder_paths, umbral_reg, img_resize = img_size)
+datasets = []
+
+dataloaders = modutils.formDataloaders(datasets, number_projections, total_size, projections_augment_factor, transform_augment_factor, train_factor, val_factor, test_factor, img_size, batch_size, tensor_path = tensor_path, load_tensor = True, save_tensor = False)
+
 train_infos = {}
 test_loss_dict = {}
 
-train_dataset, test_dataset = modutils.formRegDatasets(folder_paths, umbral_reg, img_resize = img_size)
-
 lambdas = [0.05]
-train_name = 'NBN_Lambdas_Test15'
+train_name = 'NBN_DataAugmentation_Test18'
 
 for lam in lambdas:
     
     # Load desired and undersampled datasets, on image space. Testing on Test Dataset
-    dataloaders = modutils.formDataloaders(train_dataset, test_dataset, proj_num, train_size, val_size, test_size, batch_size, img_size, augment_factor)
-    
     #%% Model Settings
     nLayer= 4
     K = 10
     epochs = 20
     max_angle = 640
     
-    model = modl.OPTmodl(nLayer, K, max_angle, proj_num, img_size, None, lam, True, results_folder)
+    model = modl.OPTmodl(nLayer, K, max_angle, number_projections, img_size, None, lam, True, results_folder)
     loss_fn = torch.nn.MSELoss(reduction = 'sum')
     loss_fbp_fn = torch.nn.MSELoss(reduction = 'sum')
     loss_backproj_fn = torch.nn.MSELoss(reduction = 'sum')
@@ -76,18 +81,18 @@ for lam in lambdas:
     #print('Second training, K = 10')
     #model.K = 10
     #K = 10
-    #epochs = 30
-    #model, train_info = modutils.model_training(model, loss_fn, loss_fbp_fn, optimizer, dataloaders, device, results_folder, num_epochs = epochs, disp = True, do_checkpoint = 0)
+    #epochs = 20
+    #model, train_info = modutils.model_training(model, loss_fn, loss_backproj_fn, loss_fbp_fn, optimizer, dataloaders, device, results_folder, num_epochs = epochs, disp = True, do_checkpoint = 0, title = train_name, plot_title = True)
     
     #train_infos[K] = train_info
 
     ##%% save loss for fbp and modl network
-    with open(results_folder+train_name+'Dict_Proj{}_nlay{}_epochs{}_K{}_lam{}_trnSize{}.pkl'.format(proj_num, nLayer, epochs, K, lam, train_size), 'wb') as f:
+    with open(results_folder+train_name+'Dict_Proj{}_nlay{}_epochs{}_K{}_lam{}_trnSize{}.pkl'.format(number_projections, nLayer, epochs, K, lam, train_factor), 'wb') as f:
     #
         pickle.dump(train_infos, f)
-        print('Diccionario salvado para proyección {}'.format(proj_num))
+        print('Diccionario salvado para proyección {}'.format(number_projections))
     #
-    modutils.save_net(model_folder+'Lambdas_K_{}_lam_{}_nlay_{}_proj_{}_trnSize{}'.format(K, lam, nLayer, proj_num, train_size), model)
+    modutils.save_net(model_folder+'Lambdas_K_{}_lam_{}_nlay_{}_proj_{}_trnSize{}'.format(K, lam, nLayer, number_projections, train_factor), model)
 
     ### Testing part
     test_loss_total = []
@@ -102,13 +107,13 @@ for lam in lambdas:
         test_loss_total.append(modutils.psnr(img_size, loss_test.item(), 1))
         test_loss_fbp_total.append(modutils.psnr(img_size, loss_test_fbp.item(), 1))
     
-    modutils.plot_outputs(target, pred, results_folder+train_name+'Test_images_proj{}.pdf'.format(proj_num))
+    modutils.plot_outputs(target, pred, results_folder+train_name+'Test_images_proj{}.pdf'.format(number_projections))
 
-    test_loss_dict[proj_num] = {'loss_net': test_loss_total, 'loss_fbp': test_loss_fbp_total}
+    test_loss_dict[number_projections] = {'loss_net': test_loss_total, 'loss_fbp': test_loss_fbp_total}
 
-    with open(results_folder+train_name+'Proj{}_nLay{}_epochs{}_K{}_lam{}_trnSize{}.pkl'.format(proj_num, nLayer, epochs, K, lam, train_size), 'wb') as f:
+    with open(results_folder+train_name+'Proj{}_nLay{}_epochs{}_K{}_lam{}_trnSize{}.pkl'.format(number_projections, nLayer, epochs, K, lam, train_factor), 'wb') as f:
         
         pickle.dump(test_loss_dict, f)
-        print('Diccionario salvado para proyección {}'.format(proj_num))
+        print('Diccionario salvado para proyección {}'.format(number_projections))
 
     del model

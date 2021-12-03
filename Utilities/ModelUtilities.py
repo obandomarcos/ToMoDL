35754943c46964ea5047caf9ed0194f4334132be
Marcos Obando <obandomarcos96@gmail.com>
2021-12-03 15:06:41 -0300

    Modify for register all datasets

--- Utilities/ModelUtilities.py
+++ Utilities/ModelUtilities.py
@@ -14,6 +14,7 @@ import math
 import matplotlib.pyplot as plt
 import cv2 
 import torchvision.transforms as T
+import pickle
 # import albumentations
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")

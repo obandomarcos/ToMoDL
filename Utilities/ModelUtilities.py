1d82ab0d0b83a6b2dfe62845a3069130afc2f7f1
Marcos Obando <obandomarcos96@gmail.com>
2021-12-03 16:10:53 -0300

    Modify for register all datasets

--- Utilities/ModelUtilities.py
+++ Utilities/ModelUtilities.py
@@ -502,7 +502,7 @@ def plot_sample(X, title):
 
     for i, ax in zip(rand, axs):
 
-        ax.imshow(X[i,0,:,:], cmap = 'gray')
+        ax.imshow(X.detach().cpu().numpy()[i,0,:,:], cmap = 'gray')
 
     fig.savefig(results_folder+title+'.pdf')
 

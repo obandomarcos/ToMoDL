95e154564a53cfb5b39cfa754b3e703c667c6e05
marcoso96 <obandomarcos96@gmail.com>
2022-03-02 14:23:13 -0300

    Training ADMM

--- Training/Training_ADMM.py
+++ Training/Training_ADMM.py
@@ -75,7 +75,8 @@ for i, (imageX_test, imageY_test, imageFiltX_test) in enumerate(zip(fullX, fullX
     sino = hR(imageY_test)
     img_rec_FBP = hRT(sino) 
     img_rec_ADMM,_,_,_ = RecTV.ADMM(y = sino, A = hR, AT = hRT, Den = Psi, alpha = 10000, delta = 1, max_iter = 200, phi = Phi, tol = 10e-7, invert = 0, warm = 0, true_img = imageY_test)
-    
+    img_rec_ADMM = (img_rec_ADMM-img_rec_ADMM.min())/(img_rec_ADMM.max()-img_rec_ADMM.min())
+
     mse = ((imageFiltX_test - img_rec_ADMM)**2).sum()
     psnr = round(modutils.psnr(img_size, mse, 1), 3)
     loss_test_ADMM.append(psnr)

55390cad5a236ae65abd95e7880ddeccfa4e161d
marcoso96 <marcos.obando@ib.edu.ar>
2022-08-18 13:07:47 -0300

    [UPDATE] Auto-install for torch-radon v2.0.0

--- environment_setup.sh
+++ environment_setup.sh
@@ -15,11 +15,7 @@ else
     conda env create -f deepopt.yml
 fi
 
-cd torch-radon
-git checkout v2
-git pull
-python make.py --local
-python setup.py install
+wget -qO- https://raw.githubusercontent.com/matteo-ronchetti/torch-radon/v2/auto_install.py | python -
 
 conda activate deepopt
 echo >> 'Environment created succesfully'

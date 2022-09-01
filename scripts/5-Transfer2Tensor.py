1c782b08708038f7715503c2933c8e146df512dd
marcoso96 <obandomarcos96@gmail.com>
2022-09-01 12:06:47 -0300

    [UPDATE] Config files for tensor dataset transform

--- scripts/5-Transfer2Tensor.py
+++ scripts/5-Transfer2Tensor.py
@@ -1,28 +1,38 @@
 import os 
 import cv2 
 import torch
+from config import *
 
 def normalize_image(image):
 
     return (image - image.min())/(image.max()-image.min())
 
-folder_paths = [x[0] for x in list(os.walk('/home/obanmarcos/Balseiro/DeepOPT/datasets/x10')) if 'red' in x[0]]
-imgs_jpg_paths = []
-imgs_torch_paths = []
+acceleration_factors = np.arange(2, 34, 2).astype(int)
 
-for folder_path in folder_paths:
-
-    imgs_jpg_paths += [folder_path+'/'+x for x in os.listdir(folder_path)]
-
-for x in imgs_jpg_paths:
-
-    imgs_torch_paths.append(x.replace('jpg', 'pt'))
-
-for str_img, str_torch in zip(imgs_jpg_paths, imgs_torch_paths):
-
-    print('written ', str_torch)
-    img = cv2.imread(str_img, cv2.IMREAD_GRAYSCALE)
-
-    img_tensor = torch.FloatTensor(img).unsqueeze(0)
+for acc_factor in acceleration_factors:
     
-    torch.save(img_tensor, str_torch)
+    path_dir = where_am_i(path = 'datasets')+'/datasets/x{}'.format(acc_factor)
+    if os.path.isdir(path_dir) == True:
+        
+        print('Trainsforming {} factor to tensor format...'.format(acc_factor))
+        
+        folder_paths = [x[0] for x in list(os.walk(path_dir)) if 'red' in x[0]]
+        imgs_jpg_paths = []
+        imgs_torch_paths = []
+
+        for folder_path in folder_paths:
+
+            imgs_jpg_paths += [folder_path+'/'+x for x in os.listdir(folder_path)]
+
+        for x in imgs_jpg_paths:
+
+            imgs_torch_paths.append(x.replace('jpg', 'pt'))
+
+        for str_img, str_torch in zip(imgs_jpg_paths, imgs_torch_paths):
+
+            print('written ', str_torch)
+            img = cv2.imread(str_img, cv2.IMREAD_GRAYSCALE)
+
+            img_tensor = torch.FloatTensor(img).unsqueeze(0)
+            
+            torch.save(img_tensor, str_torch)
--- scripts/config.py
+++ scripts/config.py
@@ -4,15 +4,19 @@ import socket
 marcos_computer_path = '/home/obanmarcos/Balseiro/DeepOPT' 
 german_computer_path = '/home/marcos/DeepOPT'
 ariel_computer_path = '/home/marcos/DeepOPT'
+ariel_computer_path = '/Datos/DeepOPT'
 
-def where_am_i():
+def where_am_i(path):
 
     if socket.gethostname() in ['copote', 'copito']:
         return marcos_computer_path
     elif socket.gethostname() == 'cabfst42':
         return german_computer_path
     elif socket.gethostname() == 'gpu1':
-        return ariel_computer_path
+        if path == 'datasets':
+            return ariel_computer_path_datasets
+        else:   
+            return ariel_computer_path
     else:
         print('Computer not found')
         sys.exit(0)

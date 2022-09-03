5c552f46bcf17407fd218017ad03f107732d205b
marcoso96 <marcos.obando@ib.edu.ar>
2022-09-03 10:54:40 -0300

    [UPDATE] Scripts for sweeps #21

--- scripts/2-Sweeps.py
+++ scripts/2-Sweeps.py
@@ -28,12 +28,14 @@ from torchvision import transforms as T
 from pytorch_msssim import SSIM
 import wandb
 
+# from torchmetrics import StructuralSimilarityIndexMeasure as SSIM
+from torchmetrics import MultiScaleStructuralSimilarityIndexMeasure as MSSSIM
+
 # Options for folding menu
 use_default_model_dict = True
 use_default_dataloader_dict = True
 use_default_trainer_dict = True
 
-# Model dictionary
 if use_default_model_dict == True:
     # ResNet dictionary parameters
     resnet_options_dict = {'number_layers': 8,
@@ -46,11 +48,12 @@ if use_default_model_dict == True:
                         'init_method': 'xavier'}
 
     # Model parameters
-    modl_dict = {'use_torch_radon': True,
+    modl_dict = {'use_torch_radon': False,
                 'number_layers': 8,
                 'K_iterations' : 8,
                 'number_projections_total' : 720,
                 'number_projections_undersampled' : 72, 
+                'acceleration_factor':10,
                 'image_size': 100,
                 'lambda': 0.05,
                 'use_shared_weights': True,
@@ -62,10 +65,11 @@ if use_default_model_dict == True:
     # Training parameters
     loss_dict = {'loss_name': 'psnr',
                 'psnr_loss': torch.nn.MSELoss(reduction = 'mean'),
-                'ssim_loss': SSIM(data_range = 1, size_average= True, channel = 1)}
+                'ssim_loss': SSIM(data_range=1, size_average=True, channel=1),
+                'msssim_loss': MSSSIM(kernel_size = 1)}
 
     # Optimizer parameters
-    optimizer_dict = {'optimizer_name': 'Adam',
+    optimizer_dict = {'optimizer_name': 'Adam+Tanh',
                     'lr': 1e-4}
 
     # System parameters
@@ -78,30 +82,47 @@ if use_default_model_dict == True:
 
 # PL Trainer and W&B logger dictionaries
 if use_default_trainer_dict == True:
-    
+            
     logger_dict = {'project':'deepopt',
                     'entity': 'omarcos', 
                     'log_model': True}
 
     lightning_trainer_dict = {'max_epochs': 40,
-                                'log_every_n_steps':1000,
-                                'gradient_clip_val' : 1.0,
+                                'log_every_n_steps': 10,
+                                'check_val_every_n_epoch': 1,
+                                'gradient_clip_val' : 0.5,
                                 'accelerator' : 'gpu', 
                                 'devices' : 1,
+                                'fast_dev_run' : False,
                                 'default_root_dir': model_folder}
+
+    profiler = None
+    # profiler = SimpleProfiler(dirpath = './logs/', filename = 'Test_training_profile_pytorch')
+    # profiler = PyTorchProfiler(dirpath = './logs/', filename = 'Test_training_profile_pytorch')
 
     trainer_dict = {'lightning_trainer_dict': lightning_trainer_dict,
                     'use_k_folding': True, 
-                    'k_fold_number_datasets': 2,
+                    'track_checkpoints': False,
+                    'epoch_number_checkpoint': 10,
+                    'use_swa' : False,
+                    'use_accumulate_batches': False,
+                    'k_fold_number_datasets': 3,
                     'use_logger' : True,
+                    'resume':'allow',
                     'logger_dict': logger_dict,
-                    'track_default_checkpoints' : True}
+                    'track_default_checkpoints'  : False,
+                    'use_auto_lr_find': False,
+                    'batch_accumulate_number': 3,
+                    'use_mixed_precision': False,
+                    'batch_accumulation_start_epoch': 0, 
+                    'profiler': profiler}
 
 # Dataloader dictionary
 if use_default_dataloader_dict == True:
     
-    data_transform = T.Compose([T.ToTensor()])
-                                
+    # data_transform = T.Compose([T.ToTensor()])
+    data_transform = None                                    
+    
     dataloader_dict = {'datasets_folder': datasets_folder,
                         'number_volumes' : 0,
                         'experiment_name': 'Bassi',
@@ -110,13 +131,15 @@ if use_default_dataloader_dict == True:
                         'save_shifts':False,
                         'number_projections_total': 720,
                         'number_projections_undersampled': 72,
+                        'acceleration_factor':10,
                         'train_factor' : 0.8, 
                         'val_factor' : 0.2,
                         'test_factor' : 0.2, 
-                        'batch_size' : 5, 
+                        'batch_size' : 8, 
                         'sampling_method' : 'equispaced-linear',
                         'shuffle_data' : True,
-                        'data_transform' : data_transform}
+                        'data_transform' : data_transform,
+                        'num_workers' : 12}
 
 hyperparameter_defaults = {'trainer_kwdict': trainer_dict,
                             'dataloader_kwdict' : dataloader_dict, 'model_system_kwdict': model_system_dict}

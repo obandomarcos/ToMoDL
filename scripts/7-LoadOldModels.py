bc3c4d7fde20d90e8509153700a8b942913177ed
marcoso96 <marcos.obando@ib.edu.ar>
2023-08-27 15:21:13 -0300

    rm folders restructure

--- scripts/7-LoadOldModels.py
+++
@@ -1,8 +0,0 @@
-import torch 
-from torchsummary import summary
-
-model_path_old = '/home/obanmarcos/Balseiro/DeepOPT/SavedModels/KFolding_PSNR_MODL_Test71K_8_lam_0.05_nlay_8_proj_72__Kfold0.pth'
-
-model = torch.load(model_path_old)
-
-print(model.keys())

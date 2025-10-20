1d617f80d6c0f8d37f59a838b75a176e93c70995
Minh Nhat Trinh <nhatthdyh@gmail.com>
2025-10-20 01:56:08 +0100

    Increment version to 0.2.22 in setup.py, update advanced model and Figure1 assets, and refactor ReconstructionWidget layout for improved UI handling.

--- napari-tomodl/setup.py
+++ napari-tomodl/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="napari-tomodl",
-    version="0.2.21",
+    version="0.2.22",
     description="A plugin for optical projection tomography reconstruction with model-based neural networks.",
     long_description=long_description,
     long_description_content_type="text/markdown",
